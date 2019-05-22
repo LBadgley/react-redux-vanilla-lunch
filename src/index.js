8a86e85bc4217a8557a899d9a3f4941625071565
Laura Badgley <l.badgleyy@gmail.com>
2019-05-22 14:45:57 -0700

    index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  lunchBox: 'empty'
+  
 };
 
 function reducer(state = initialState, action) {
   switch(action.type) {
-    case: 'ADD_DRINK':
-      return { payload: 'malbec' };
-      default: 
-        return state;
+    case 'ADD_DRINK':
+      return { ...state, drink: action.payload };
+    default: 
+      return state;
   }
 }
 const store = createStore(reducer);
+
+store.dispatch({ 
+  type: 'ADD_DRINK',
+  payload: 'Malbec'
+});
+
+console.log('added drank', store.getState());
