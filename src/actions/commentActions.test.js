c8a5b5b7a9b247b640e6739baa13c3fcde554a99
Laura Badgley <l.badgleyy@gmail.com>
2019-05-23 12:57:24 -0700

    handles the createComment with existing comment test passing

--- src/actions/commentActions.test.js
+++ src/actions/commentActions.test.js
@@ -1,13 +1,13 @@
 import { createComment, CREATE_COMMENT, deleteComment, DELETE_COMMENT } from './commentActions';
 
 describe('comment actions test', () => {
-  it('handles the CREATE_COMMENT action', () => {
+  it('handles the createComment action', () => {
     expect(createComment(0, 'My comment')).toEqual({
       type: CREATE_COMMENT,
       payload: { postId: 0, comment: 'My comment' }
     });
   });
-  it('handles the DELETE_COMMENT action', () => {
+  it('handles the deleteComment action', () => {
     expect(deleteComment(0, 1)).toEqual({
       type: DELETE_COMMENT,
       payload: { postId: 0, commentId: 1 }
---
+++ src/reducers/commentReducer.test.js
@@ -0,0 +1,23 @@
+import reducer from './commentReducer';
+import { createComment, deleteComment } from '../actions/commentActions';
+import { deletePost } from '../actions/postActions';
+
+describe('comment reducer tests', () => {
+  it('handles the CREATE_COMMENT action', () => {
+    const newState = reducer({}, createComment(1, 'Is this a comment?'));
+
+    expect(newState).toEqual({
+      1: ['Is this a comment?']
+    });
+  });
+  it('handles the CREATE_COMMENT with existing comments', () => {
+    const initialState = {
+      1: ['hi']
+    };
+    const newState = reducer(initialState, createComment(1, 'Is this a comment?'));
+
+    expect(newState).toEqual({
+      1: ['hi', 'Is this a comment?']
+    });
+  });
+});
