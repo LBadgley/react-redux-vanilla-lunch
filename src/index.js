a4a62eb79b9ca94a08bde62d9f89ad6ae06baa85
Laura Badgley <l.badgleyy@gmail.com>
2019-05-22 21:08:51 -0700

    messed with index.js... not sure if it's right?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
 
 render (
-  <App />,
+  <createStore />,
   document.getElementById('root')
 );
