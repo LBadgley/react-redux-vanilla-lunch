9add81c7fce9db6ad80ad7c81378ba70479cc600
Laura Badgley <l.badgleyy@gmail.com>
2019-05-23 18:00:00 -0700

    initial commit for blog-react

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h1>hello this is the h1 you made</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-// import reducer from './reducers';
-// import { createStore } from 'redux';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-// const store = createStore(reducer);
-
-// store.dispatch({})
+render (
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
