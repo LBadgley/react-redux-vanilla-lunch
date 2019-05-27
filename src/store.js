24ef590d0107b4cc01244af28a36c03f32b14101
Laura Badgley <l.badgleyy@gmail.com>
2019-05-27 08:46:27 -0700

    fixed import pathing

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
-import reducer from './reducers';
-
+import reducer from './reducers/postReducer';
 export default createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
