e43e859f6a02f3bd3329e0aa125c92eb44460c2d
Laura Badgley <l.badgleyy@gmail.com>
2019-05-27 08:48:48 -0700

    added styles to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import CreatePost from '../containers/post/CreatePost';
 import AllPosts from '../containers/post/AllPosts';
-// import styles from './Home.css';
+import styles from './Home.css';
 
 export default function Home() {
   return (
     <>
-      <section >
+      <section className={styles.AllPosts}>
         <h1>Posts</h1>
         <AllPosts />
       </section>
-      <section>
+      <section className={styles.CreatePost}>
         <h2>Write A Post</h2>
         <CreatePost />
       </section>
